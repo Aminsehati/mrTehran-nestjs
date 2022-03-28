a8028b24ec9f9379f25f2ed415fa2cd03599b0ff
Amin sehati <aminsehati70@gmail.com>
2022-03-28 22:04:29 +0430

    change file schema.gql

--- src/schema/graphql.ts
+++ src/schema/graphql.ts
@@ -5,10 +5,21 @@
  * -------------------------------------------------------
  */
 
-// import { createPlayList } from "src/playList/input/createPlayList.input";§
+import { createPlayList } from "src/playList/input/createPlayList.input";
 
 /* tslint:disable */
 /* eslint-disable */
+export class CreateActorInput {
+    name?: Nullable<string>;
+    imgUrl?: Nullable<string>;
+    coverImgUrl?: Nullable<string>;
+}
+
+export class Pagination {
+    limit?: Nullable<number>;
+    skip?: Nullable<number>;
+}
+
 export class UpdatePlayListInput {
     name: string;
     imgUrl: string;
@@ -21,10 +32,38 @@ export class CreatePlayList {
     coverImgUrl?: Nullable<string>;
 }
 
+export class Artist {
+    _id?: Nullable<string>;
+    name?: Nullable<string>;
+    imgUrl?: Nullable<string>;
+    coverImgUrl?: Nullable<string>;
+    Followers?: Nullable<number>;
+}
+
 export abstract class IQuery {
-    abstract getPlayLists(): Nullable<Nullable<PlayList>[]> | Promise<Nullable<Nullable<PlayList>[]>>;
+    abstract getArtists(): Nullable<Nullable<Artist>[]> | Promise<Nullable<Nullable<Artist>[]>>;
+
+    abstract getArtist(id: string): Nullable<Artist> | Promise<Nullable<Artist>>;
+
+    abstract getArtistsCount(): number | Promise<number>;
+
+    abstract getPlayLists(pagination?: Nullable<Pagination>): Nullable<Nullable<PlayList>[]> | Promise<Nullable<Nullable<PlayList>[]>>;
 
     abstract getPlayList(id: string): Nullable<PlayList> | Promise<Nullable<PlayList>>;
+
+    abstract getPlayListsCount(): number | Promise<number>;
+}
+
+export abstract class IMutation {
+    abstract createActor(input?: Nullable<CreateActorInput>): Nullable<Artist> | Promise<Nullable<Artist>>;
+
+    abstract createPlayList(input?: Nullable<createPlayList>): Nullable<PlayList> | Promise<Nullable<PlayList>>;
+
+    abstract updatePlayList(id: string, input: UpdatePlayListInput): Nullable<PlayList> | Promise<Nullable<PlayList>>;
+
+    abstract deletePlayList(id: string): Nullable<PlayList> | Promise<Nullable<PlayList>>;
+
+    abstract FollowPlayList(id: string): Nullable<PlayList> | Promise<Nullable<PlayList>>;
 }
 
 export class PlayList {
@@ -33,14 +72,6 @@ export class PlayList {
     imgUrl?: Nullable<string>;
     coverImgUrl?: Nullable<string>;
     Followers?: Nullable<number>;
-}
-
-export abstract class IMutation {
-    abstract createPlayList(input?: Nullable<CreatePlayList>): Nullable<PlayList> | Promise<Nullable<PlayList>>;
-
-    abstract updatePlayList(id: string, input: UpdatePlayListInput): Nullable<PlayList> | Promise<Nullable<PlayList>>;
-
-    abstract deletePlayList(id: string): Nullable<PlayList> | Promise<Nullable<PlayList>>;
 }
 
 type Nullable<T> = T | null;
